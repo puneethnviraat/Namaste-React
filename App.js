1c6e953f78348847e884de113e223b10db507177
Puneeth N <34005393+puneethnviraat@users.noreply.github.com>
2023-02-01 08:45:03 +0530

    assignemt -3

--- App.js
+++ App.js
@@ -1,9 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-    const heading1= React.createElement('h1',{ id:"rddoot", style:{color:"red"}}, "this is for testing.11.")
-    const heading2= React.createElement('h3',{ id:"rddeot", style:{color:"green"}}, "this is for testing..22")
-    const container= React.createElement('div',{class:"container"}, [heading1,heading2])
+import logo from './assets/your-puneeth-logo.png';
+import { FcBusinessman } from "react-icons/fc";
+
+// nested header element using JSX
+const title1= React.createElement("h1",{style:{color:"red"}},"Test divle 1");
+const title2= React.createElement('h2', {id:"testdivne"},"test divne Two this is for testing");
+const title3= React.createElement(
+    "h3",{},"This is the assingment three did for Namaste react project.."
+    );
+    const TitleComponent1= React.createElement(
+        "div",{className:"title"},[title1,title2,title3]      
+        )
+    
+// nested header element using functional component
+const TitleComponent2=()=>{
+    
+    return(
+        <div className="title" >
+            {TitleComponent1}
+        <h1>Test divle 2</h1>
+        <h2>test divne Two this is for testing of functional component</h2>
+        <h3>This is the assingment three did for Namaste react project..</h3>
+        </div>
+    )
+}
+const TitleComponent3=()=>{
+    
+    
+    return(
+        <div className="title" >
+            <div>
+            <img className="logo" src={logo} alt="" />
+
+            </div>
+        <div >
+        <input className="search" type="text" placeholder="Search here"/>
+        </div>
+    <div>
+    <FcBusinessman style={{ fontSize: "2.5em" }}/> User name
+    </div>
+        </div>
+    )
+}
+
+//New header component using functional component
+
 
 
     const root = ReactDOM.createRoot(document.getElementById("root"))
-    root.render(container);
+    root.render(<TitleComponent3 />);
+
